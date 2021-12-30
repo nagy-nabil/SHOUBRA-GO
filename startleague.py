e321b9a7b930eef0be4d01033896e84d943485f2
omageza <66173903+omageza@users.noreply.github.com>
2021-12-31 01:25:50 +0200

    Update startleague.py

--- startleague.py
+++ startleague.py
@@ -1,14 +1,14 @@
 
-from typing import MutableMapping
 from club import Club
 from player import Player
-from club import not_empty_entry
+from club import not_empty_entry,show_logo
 from tkinter import *
 from random import shuffle
 from tkinter import messagebox
 import csv
 import os
 from tkinter.ttk import *
+import tkinter.scrolledtext as st
 from club import open_link
 #list of clubs names
 clubs=[]
@@ -100,15 +100,14 @@ def new_league_dict():
                 clubs_objs[team].add_player(Player(player[0],int(player[1]),player[2]))
                 # clubs_objs[team].add_player(clubs_objs[team].create_player(player[0],int(player[1]),player[2]))
         # print(f"{team} in start= {len(clubs_objs[team].get_squad())}")
-
 #function to print match schulde as gui
-def table_gui(master,i,league_name,state=NORMAL):
+def table_gui(master,frame,i,league_name,state=NORMAL):
     #show all gui for the table
-    team1=Label(master,text=f"{table[i][0]}")
-    team1_entry=Entry(master,state=state)
-    team2_entry=Entry(master,state=state)
-    x_button=Button(master,text="X",state=state,command=lambda:x_command(team1_entry,team2_entry,master,x_button,league_name))
-    team2=Label(master,text=f"{table[i][1]}")
+    team1=Label(frame,text=f"{table[i][0]}")
+    team1_entry=Entry(frame,state=state)
+    team2_entry=Entry(frame,state=state)
+    x_button=Button(frame,text="X",state=state,command=lambda:x_command(team1_entry,team2_entry,master,frame,x_button,league_name))
+    team2=Label(frame,text=f"{table[i][1]}")
     team1.grid(row=i+1,column=1,padx=10,pady=10)
     team1_entry.grid(row=i+1,column=2,padx=10,pady=10)
     x_button.grid(row=i+1,column=3,padx=10,pady=10)
@@ -116,7 +115,7 @@ def table_gui(master,i,league_name,state=NORMAL):
     team2_entry.grid(row=i+1,column=5,padx=10,pady=10)
     team2.grid(row=i+1,column=6,padx=10,pady=10)
 
-def table_gui_dis(master,i,league_name):
+def table_gui_dis(master,i):
     #show all gui for the table
     team1=Label(master,text=f"{table[i][0]}")
     team1_entry=Entry(master)
@@ -125,7 +124,7 @@ def table_gui_dis(master,i,league_name):
     team2_entry.insert(0,f"{table[i][3]}")
     team1_entry.config(state=DISABLED)
     team2_entry.config(state=DISABLED)
-    x_button=Button(master,text="X",state=DISABLED,command=lambda:x_command(team1_entry,team2_entry,master,x_button,league_name))
+    x_button=Button(master,text="X",state=DISABLED)
     team2=Label(master,text=f"{table[i][1]}")
     team1.grid(row=i+1,column=1,padx=10,pady=10)
     team1_entry.grid(row=i+1,column=2,padx=10,pady=10)
@@ -133,9 +132,17 @@ def table_gui_dis(master,i,league_name):
     team2.grid(row=i+1,column=4,padx=10,pady=10)
     team2_entry.grid(row=i+1,column=5,padx=10,pady=10)
     team2.grid(row=i+1,column=6,padx=10,pady=10)
-
+#functions show and perform what to do after the league is over
+def do_after_over(master,league_name):
+    league_done_frame=Frame(master)
+    Label(league_done_frame,text="your league is over").grid(row=1 , padx=10, pady=10)
+    Button(league_done_frame,text="Standing",command=scoreboard).grid(row=2 ,column=0, padx=10, pady=10)
+    Button(league_done_frame,text="end league",command=lambda:end_league(master,league_name)).grid(row=2 ,column=1, padx=10, pady=10)
+    Button(league_done_frame,text="EXIT",command=master.destroy).grid(row=2 ,column=2, padx=10, pady=10)
+    league_done_frame.pack()
 #used to disable x button and 2 entry and create new table line(next match)
-def x_command(e1,e2,master,button,league_name):
+#master here will be frame (table_frame widgets with grid)
+def x_command(e1,e2,master,frame,button,league_name):
     global i
     global clubs_objs
     global table
@@ -155,14 +162,15 @@ def x_command(e1,e2,master,button,league_name):
         #recall table gui to create next line of the table 
         i+=1
         if i<len(table):
-            table_gui(master,i,league_name)
+            table_gui(master,frame,i,league_name)
         else:
             #what to do after the league is over
-            Label(master,text="your league is over").grid(row=100 , padx=10, pady=10)
-            Button(master,text="EXIT",command=master.destroy).grid(row=101 ,column=1, padx=10, pady=10)
-            Button(master,text="end league",command=lambda:end_league(master,league_name)).grid(row=101 ,column=3, padx=10, pady=10)
-            Button(master,text="Standing",command=scoreboard).grid(row=101 ,column=2, padx=10, pady=10)
-            #Button(text="exit",command=master.destroy).grid(row=100) #exit button
+            # Label(master,text="your league is over").grid(row=100 ,column=1,columnspan=2, padx=10, pady=10)
+            # Button(master,text="EXIT",command=master.destroy).grid(row=101 ,column=4, padx=10, pady=10)
+            # Button(master,text="end league",command=lambda:end_league(master,league_name)).grid(row=101 ,column=3, padx=10, pady=10)
+            # Button(master,text="Standing",command=scoreboard).grid(row=101 ,column=1,columnspan=2, padx=10, pady=10)
+            # #Button(text="exit",command=master.destroy).grid(row=100) #exit button
+            do_after_over(master,league_name)
     else:
         messagebox.showerror("VALID", "NON VAILD SCORE")
         e1.delete(0,END)
@@ -202,12 +210,12 @@ def modify_file(name):
         # print(f"in mod after {table[i][1]} {len(squad)}")
 
 #continue league know where we are in table 
-def con_league(master,league_name):
+def con_league(master):
     global i
     for match in table:
         #result for team 1 
         if match[2] != -1:
-            table_gui_dis(master,i,league_name)
+            table_gui_dis(master,i)
             i+=1
         else:
             return
@@ -294,6 +302,7 @@ def button_plyers_data(teamName,master,i):
 def display_squad(obj,master):
     # global master
     squad_page=Toplevel(master)
+    squad_page.title(f"{obj.get_name()}")
     # Label(master,text="PLAYER NAME\t\t\t\t\t\tAGE\t\t\tPOSITION\t\t\tNO GOALS\t\t\tYEL CARD\t\t\tRED CARDS").grid(row=0,padx=10,pady=10)
     Label(squad_page,text="PLAYER NAME").grid(row=0,padx=10,pady=10,column=0)
     Label(squad_page,text="AGE").grid(row=0,padx=10,pady=10,column=1)
@@ -313,12 +322,17 @@ def display_squad(obj,master):
         i+=1
     # master.mainloop()
 
-#to end current league by deleting file league and close window
+#to end current league by deleting file league and close window and after ending the league add those clubs to avaliable clubs again
 def end_league(master,name):
     if os.path.exists(f"{name}.csv"):
         sure=messagebox.askyesno("DELETE","are you sure you want to delete your league?")
         if sure:
             os.remove(f"{name}.csv")
+            #to add clubs in avaliable
+            with open(f'{os.getcwd()}/available.csv','a',newline='') as csvfile:
+                writer=csv.writer(csvfile,delimiter=',',quotechar='|')
+                for club in clubs:
+                    writer.writerow([club])
             messagebox.showinfo("DELETED",f"{name} deleted successfully")
             master.destroy()
     else:
@@ -333,7 +347,9 @@ def main_menu(master,name):
     menubar.add_cascade(label="file",menu=filemenu)
     filemenu.add_command(label="add club",command=lambda:Club.add_club(master))
     filemenu.add_command(label="delete club",command=lambda:Club.delete_club_gui(master))
+    filemenu.add_separator() 
     filemenu.add_command(label="standing",command=lambda:scoreboard())
+    filemenu.add_command(label="matches table",command=lambda:show_matches_table())
     filemenu.add_command(label="end league",command=lambda:end_league(master,name))
     filemenu.add_separator() 
     #to close master window in any time
@@ -362,6 +378,30 @@ def clubs_database():
         print("No Error")
     except:
         print("Error")
+#to print the table of matches
+def show_matches_table():
+    master=Tk()
+    master.config(background="white")
+    master.title("Matches Table")
+    master.geometry("500x500")
+    master.resizable(False,False)
+    #Text to show all matches with scrollbar
+    text=st.ScrolledText(master,height=400, bd=0,background="white" ,font = ("Times New Roman",15),highlightthickness=0, relief='ridge')
+    # scrollbar = Scrollbar(master,orient="vertical", command=text.yview)
+    # scrollbar.pack( side = RIGHT, fill = Y )
+    #text.bind("<Configure>", lambda event, text=text: onFrameConfigure(text))
+    
+    for match in range(len(table)):
+        # Label(text,text=f"match number {match}:\n{table[match][0]} \t VS \t {table[match][1]}",background="white").pack(side=TOP)
+        text.insert(INSERT,f"match number {match+1}:\n{table[match][0]} \t VS \t {table[match][1]}\n")
+    text.config(state=DISABLED)
+    Button(master,text="EXIT",command=master.destroy).pack(padx=10,pady=10)
+    text.pack(fill=Y)
+    # text.configure(yscrollcommand=scrollbar.set)
+    
+def onFrameConfigure(canvas):
+    '''Reset the scroll region to encompass the inner frame'''
+    canvas.configure(scrollregion=canvas.bbox("all"))
 #to start new league
 def start_league_gui(window):
     window.destroy()
@@ -372,16 +412,28 @@ def start_league_gui(window):
     
     master=Tk()
     master.iconbitmap('4876628.ico')
-    master.geometry("750x600")
-    league_name_label=Label(master,text="league name")
-    league_name_entry=Entry(master)
-    league_name_button=Button(master,text="start",command=lambda:start_league(master,league_name_entry,league_name_button))
+    master.geometry("750x750")
+    master.resizable(False,False)
+    master.title("start league")
+    #canvas to hold table gui frame to add scrollbar 
+    canvas=Canvas(master,width=550,height=300, bd=0, highlightthickness=0, relief='ridge')
+    scrollbar = Scrollbar(master,orient="vertical", command=canvas.yview)
+    scrollbar.pack( side = RIGHT, fill = Y )
+    show_logo(master,250,250)
+    start_frame=Frame(master)
+    league_name_label=Label(start_frame,text="league name")
+    league_name_entry=Entry(start_frame)
+    league_name_button=Button(start_frame,text="start",command=lambda:start_league(master,canvas,league_name_entry,league_name_button))
     league_name_label.grid(row=0, column=0,padx=10,pady=10)
     league_name_entry.grid(row=0, column=1,padx=10,pady=10)
     league_name_button.grid(row=0, column=2,padx=10,pady=10)
+    start_frame.pack()
+    #number of matches can be bigger than our geometry and frame doesn't support scroll bar so we made canvas to support scrollbar
+    canvas.configure(yscrollcommand=scrollbar.set)
+    canvas.pack(side=TOP ,expand=True,fill=Y)
     master.mainloop()
 #start new league command
-def start_league(master,name_entry,button):
+def start_league(master,canvas,name_entry,button):
     global clubs
     global clubs_no
     global table
@@ -395,48 +447,69 @@ def start_league(master,name_entry,button):
     i=0
     if not_empty_entry(name_entry):
         name=name_entry.get().strip().title()
-        name_entry.config(state=DISABLED)
-        button.destroy()
-        
-        clubs_database()#load from data base
         if f"{name}.csv" in os.listdir(os.getcwd()):
             ask=messagebox.askyesno("ASK","there's the same name do you want to load it?")
             if ask==1:
                 main_menu(master,name)
-                continue_league(master,name_entry,button)
+                continue_league(master,canvas,name_entry,button)
                 name_entry.config(state=DISABLED)
                 button.destroy()
-        #if there's no league will check for clubs number if bigger then 2 will make new league if not say not enough clubs and give button to add new club
-        #start new league
-        elif(clubs_no>2):
-            main_menu(master,name)
+        else:
+            clubs_database()#load from data base
             name_entry.config(state=DISABLED)
             button.destroy()
-            new_league_dict()
-            #create new league file
-            new_league_file(name)
-            table_gui(master,i,name)
-        elif(clubs_no<3):
-            Label(master,text="You don't Have enough clubs to start league").grid(row=1, column=0,padx=10,pady=10)
-            add_club=Button(master,text="add club",command=lambda:Club.add_club(master))
-            add_club.grid(row=3, column=1,padx=10,pady=10)
+            #if there's no league will check for clubs number if bigger then 2 will make new league if not say not enough clubs and give button to add new club
+            #start new league
+            if(clubs_no>2):
+                main_menu(master,name)
+                name_entry.config(state=DISABLED)
+                button.destroy()
+                new_league_dict()
+                #create new league file
+                new_league_file(name)
+                #frame so table gui add in it and it will be added to the canves
+                table_frame=Frame(canvas)
+                #The values (0, 0) tell the canvas on which position to draw the window. The argument anchor="nw" tells the canvas to place the 
+                # frame's top left corner on position (0, 0).
+                canvas.create_window((0,0), window=table_frame,anchor="n")
+                table_frame.bind("<Configure>", lambda event, canvas=canvas: onFrameConfigure(canvas))
+                table_gui(master,table_frame,i,name)
+            elif(clubs_no<3):
+                # Label(master,text="You don't Have enough clubs to start league").grid(row=1, column=0,padx=10,pady=10)
+                Label(master,text="You don't Have enough clubs to start league").pack()
+                add_club=Button(master,text="add club",command=lambda:Club.add_club(master))
+                add_club.pack(pady=10)
+                # add_club.grid(row=3, column=1,padx=10,pady=10)
     else:
         messagebox.showerror("EMPTY","Empty league name")
 
 def continue_league_gui(window):
     window.destroy()
     master=Tk()
+    master.title("Continue ")
+    master.geometry("750x750")
+    master.resizable(False,False)
     master.iconbitmap('4876628.ico')
-    league_name_label=Label(master,text="league name")
-    league_name_entry=Entry(master)
-    league_name_button=Button(master,text="start",command=lambda:continue_league(master,league_name_entry,league_name_button))
+    #canvas to hold table gui frame to add scrollbar 
+    canvas=Canvas(master,width=550,height=300, bd=0, highlightthickness=0, relief='ridge')
+    scrollbar = Scrollbar(master,orient="vertical", command=canvas.yview)
+    scrollbar.pack( side = RIGHT, fill = Y )
+    show_logo(master,250,250)
+    continue_frame=Frame(master)
+    league_name_label=Label(continue_frame,text="league name")
+    league_name_entry=Entry(continue_frame)
+    league_name_button=Button(continue_frame,text="start",command=lambda:continue_league(master,canvas,league_name_entry,league_name_button))
     league_name_label.grid(row=0, column=0,padx=10,pady=10)
     league_name_entry.grid(row=0, column=1,padx=10,pady=10)
     league_name_button.grid(row=0, column=2,padx=10,pady=10)
+    continue_frame.pack()
+    #number of matches can be bigger than our geometry and frame doesn't support scroll bar so we made canvas to support scrollbar
+    canvas.configure(yscrollcommand=scrollbar.set)
+    canvas.pack(side=TOP ,expand=True)
     master.mainloop()
 
 #function load data from league file and all squads files
-def continue_league(master,name_entry,button):
+def continue_league(master,canvas,name_entry,button):
     global clubs
     global clubs_no
     global table
@@ -449,28 +522,34 @@ def continue_league(master,name_entry,button):
     clubs_objs.clear()
     i=0
     if not_empty_entry(name_entry):
-        name=name_entry.get().strip().title()
-
-        if f"{name}.csv" in os.listdir(os.getcwd()):
-            main_menu(master,name)
+        league_name=name_entry.get().strip().title()
+        if f"{league_name}.csv" in os.listdir(os.getcwd()):
+            main_menu(master,league_name)
             name_entry.config(state=DISABLED)
             button.destroy()
-            load_league(name)
-            con_league(master,name)
+            load_league(league_name)
+            #frame so table gui add in it and it will be added to the canves
+            table_frame=Frame(canvas)
+            #The values (0, 0) tell the canvas on which position to draw the window. The argument anchor="nw" tells the canvas to place the 
+            # frame's top left corner on position (0, 0).
+            canvas.create_window((0,0), window=table_frame,anchor="n")
+            table_frame.bind("<Configure>", lambda event, canvas=canvas: onFrameConfigure(canvas))
+            con_league(table_frame)
             # if i reach table length means that all matches had been played so tell the user the league has ended
             if (i>=len(table)):
-                Label(master,text="your league is over").grid(row=100 , padx=10, pady=10)
-                Button(master,text="EXIT",command=master.destroy).grid(row=101 ,column=1, padx=10, pady=10)
-                Button(master,text="end league",command=lambda:end_league(master,name)).grid(row=101 ,column=3, padx=10, pady=10)
-                Button(master,text="Standing",command=scoreboard).grid(row=101 ,column=2, padx=10, pady=10)
+                # league_done_frame=Frame(master)
+                # Label(league_done_frame,text="your league is over").grid(row=100 , padx=10, pady=10)
+                # Button(league_done_frame,text="EXIT",command=master.destroy).grid(row=101 ,column=2, padx=10, pady=10)
+                # Button(league_done_frame,text="end league",command=lambda:end_league(master,league_name)).grid(row=101 ,column=1, padx=10, pady=10)
+                # Button(league_done_frame,text="Standing",command=scoreboard).grid(row=101 ,column=0, padx=10, pady=10)
+                # league_done_frame.pack()
+                do_after_over(master,league_name)
             else:
-                table_gui(master,i,name)
+                table_gui(master,table_frame,i,league_name)
         else:
             ask=messagebox.askyesno("ASK","name not avaliable do you want to create new league?")
             if ask==1:
-                main_menu(master,name)
-                name_entry.config(state=DISABLED)
-                button.destroy()
-                start_league(master,name_entry,button)
+                main_menu(master,league_name)
+                start_league(master,canvas,name_entry,button)
     else:
         messagebox.showerror("EMPTY","Empty league name")
